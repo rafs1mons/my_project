9f237725a5155c80827df7466e985284acdcec46
root <aakkaya300@gmail.com>
2024-05-10 17:36:38 +0300

    kitap model and migration

---
+++ database/migrations/2024_05_10_142445_create_kitaps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('kitaplar', function (Blueprint $table) {
+        $table->id();
+        $table->string('isim');
+        $table->string('yazar');
+        $table->string('durum'); // okunacak, elimde, alınacak
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kitaps');
+    }
+};
